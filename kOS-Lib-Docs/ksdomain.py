237103f7b4aba8659f24df6598595124f35604dd
Richard Thomas Harrison <harrison.rt@gmail.com>
2018-03-27 05:52:10 +0100

    Fixed ksdomain for newer version of Sphinx

--- kOS-Lib-Docs/ksdomain.py
+++ kOS-Lib-Docs/ksdomain.py
@@ -12,7 +12,8 @@ from sphinx.locale import l_, _
 from sphinx.domains import Domain, ObjType, Index
 from sphinx.directives import ObjectDescription
 from sphinx.util.nodes import make_refnode
-from sphinx.util.compat import Directive
+# from sphinx.util.compat import Directive
+from sphinx.util import compat
 from sphinx.util.docfields import Field, GroupedField, TypedField
 
 ks_sig_re = re.compile(r'''
