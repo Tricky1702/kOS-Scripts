5f02eab9b643cf6df3ad5425333de44fea01c975
Richard Thomas Harrison <harrison.rt@gmail.com>
2018-03-27 06:10:13 +0100

    Fix Directive import properly

--- kOS-Lib-Docs/ksdomain.py
+++ kOS-Lib-Docs/ksdomain.py
@@ -13,7 +13,7 @@ from sphinx.domains import Domain, ObjType, Index
 from sphinx.directives import ObjectDescription
 from sphinx.util.nodes import make_refnode
 # from sphinx.util.compat import Directive
-from sphinx.util import compat
+from docutils.parsers.rst import Directive
 from sphinx.util.docfields import Field, GroupedField, TypedField
 
 ks_sig_re = re.compile(r'''
